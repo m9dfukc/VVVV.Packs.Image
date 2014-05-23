8fd14e88f1854f8a42b1d19b9161e966184d2fd7
m9dfukc <andreas@invertednothing.com>
2014-05-23 14:58:01 +0200

    added a premature Debayering filter - be warned this give unrespected result f.e. in combination with resize and rotate

---
+++ src/nodes/plugins/Image/OpenCV/src/Filters/DeBayer.cs
@@ -0,0 +1,38 @@
+﻿#region using
+using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+using System;
+using VVVV.Utils.VColor;
+using VVVV.CV.Core;
+
+#endregion
+
+namespace VVVV.CV.Nodes
+{
+	[FilterInstance("DeBayer", Help = "Converts incoming Bayer L8 image into RGB", Author = "m9dfukc")]
+	public class DeBayerInstance : IFilterInstance
+    {
+        public override void Allocate()
+        {
+            FOutput.Image.Initialise(FInput.Image.ImageAttributes.Size, TColorFormat.RGB8);
+		}
+
+		public override void Process()
+		{
+            // Whenever you access the pixels directly of FInput
+            // you must lock it for reading using 
+            if (!FInput.LockForReading()) return;
+
+            CvInvoke.cvCvtColor(FInput.CvMat, FOutput.CvMat, COLOR_CONVERSION.CV_BayerBG2BGR);
+
+            FInput.ReleaseForReading(); // and this after you've finished with FImage
+            FOutput.Send();
+		}
+	}
+}
