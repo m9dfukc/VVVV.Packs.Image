65b685887ca91e72317a6bdc2e5e8525edc397fe
m9dfukc <andreas@invertednothing.com>
2014-05-25 21:27:42 +0200

    Pass through if wrong color format

--- src/nodes/plugins/Image/OpenCV/src/Filters/DeBayer.cs
+++ src/nodes/plugins/Image/OpenCV/src/Filters/DeBayer.cs
@@ -18,21 +18,48 @@ namespace VVVV.CV.Nodes
 	[FilterInstance("DeBayer", Help = "Converts incoming Bayer L8 image into RGB", Author = "m9dfukc")]
 	public class DeBayerInstance : IFilterInstance
     {
+        TColorFormat FOutFormat;
+        Boolean FPassThrough = false;
         public override void Allocate()
         {
-            FOutput.Image.Initialise(FInput.Image.ImageAttributes.Size, TColorFormat.RGB8);
+            FOutFormat = GetFormat(FInput.ImageAttributes.ColorFormat);
+
+            //if we can't convert or it's already grayscale, just pass through
+            if (FOutFormat == TColorFormat.UnInitialised)
+            {
+                FOutFormat = FInput.ImageAttributes.ColorFormat;
+                FPassThrough = true;
+            }
+                
+            FOutput.Image.Initialise(FInput.Image.ImageAttributes.Size, FOutFormat);
 		}
 
 		public override void Process()
 		{
-            // Whenever you access the pixels directly of FInput
-            // you must lock it for reading using 
-            if (!FInput.LockForReading()) return;
+            if (FPassThrough)
+                FInput.GetImage(FOutput.Image);
+            else
+            {
+                // Whenever you access the pixels directly of FInput
+                // you must lock it for reading using 
+                if (!FInput.LockForReading()) return;
+                CvInvoke.cvCvtColor(FInput.CvMat, FOutput.CvMat, COLOR_CONVERSION.CV_BayerBG2BGR);
+                FInput.ReleaseForReading(); // and this after you've finished with FImage
+            }   
 
-            CvInvoke.cvCvtColor(FInput.CvMat, FOutput.CvMat, COLOR_CONVERSION.CV_BayerBG2BGR);
-
-            FInput.ReleaseForReading(); // and this after you've finished with FImage
             FOutput.Send();
 		}
+
+        public static TColorFormat GetFormat(TColorFormat format)
+        {
+            switch (format)
+            {
+                case TColorFormat.L8:
+                    return TColorFormat.RGB8;
+
+                default:
+                    return TColorFormat.UnInitialised;
+            }
+        }
 	}
 }
