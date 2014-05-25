e0076fe9f0926255b10329a3bc40f714319326b7
m9dfukc <andreas@invertednothing.com>
2014-05-25 22:23:33 +0200

    add option to convert raw to grayscale -> a lot faster than converting to rgb

--- src/nodes/plugins/Image/OpenCV/src/Filters/DeBayer.cs
+++ src/nodes/plugins/Image/OpenCV/src/Filters/DeBayer.cs
@@ -18,14 +18,21 @@ namespace VVVV.CV.Nodes
 	[FilterInstance("DeBayer", Help = "Converts incoming Bayer L8 image into RGB", Author = "m9dfukc")]
 	public class DeBayerInstance : IFilterInstance
     {
+        COLOR_CONVERSION FConversionFormat;
         TColorFormat FOutFormat;
         Boolean FPassThrough = false;
+
+        [Input("Grayscale")]
+        public Boolean Grayscale = false;
+
         public override void Allocate()
         {
-            FOutFormat = GetFormat(FInput.ImageAttributes.ColorFormat);
+            FOutFormat = GetColorFormat(FInput.ImageAttributes.ColorFormat, Grayscale);
 
             //if we can't convert or it's already grayscale, just pass through
-            if (FOutFormat == TColorFormat.UnInitialised)
+            if (FOutFormat != TColorFormat.UnInitialised)
+                FConversionFormat = GetConversionFormat(FOutFormat);
+            else
             {
                 FOutFormat = FInput.ImageAttributes.ColorFormat;
                 FPassThrough = true;
@@ -43,19 +50,31 @@ namespace VVVV.CV.Nodes
                 // Whenever you access the pixels directly of FInput
                 // you must lock it for reading using 
                 if (!FInput.LockForReading()) return;
-                CvInvoke.cvCvtColor(FInput.CvMat, FOutput.CvMat, COLOR_CONVERSION.CV_BayerBG2BGR);
+                CvInvoke.cvCvtColor(FInput.CvMat, FOutput.CvMat, FConversionFormat);
                 FInput.ReleaseForReading(); // and this after you've finished with FImage
             }   
 
             FOutput.Send();
 		}
 
-        public static TColorFormat GetFormat(TColorFormat format)
+        public static COLOR_CONVERSION GetConversionFormat(TColorFormat format)
         {
             switch (format)
             {
                 case TColorFormat.L8:
-                    return TColorFormat.RGB8;
+                    return COLOR_CONVERSION.CV_BayerBG2GRAY;
+
+                default:
+                    return COLOR_CONVERSION.CV_BayerBG2BGR;
+            }
+        }
+
+        public static TColorFormat GetColorFormat(TColorFormat format, Boolean grayscale)
+        {
+            switch (format)
+            {
+                case TColorFormat.L8:
+                    return (grayscale ? TColorFormat.L8 : TColorFormat.RGB8);
 
                 default:
                     return TColorFormat.UnInitialised;
